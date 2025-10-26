0b558a2cf4ad9bfba8a2771f0f959d31140aa9aa
Ram-44 <srramsr1997@gmail.com>
2025-10-26 19:32:57 +0530

    bug fix

--- week 2 codes/demux.v
+++ week 2 codes/demux.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 
 module demux(
-    input [7:0]in,
+    input in,
     input [2:0]s,
     output reg out
     );
    always@(*)
    begin
    case(s)
-   3'b000:out=in[0];
-   3'b001:out=in[1];
-   3'b010:out=in[2];
-   3'b011:out=in[3];
-   3'b100:out=in[4];
-   3'b101:out=in[5];
-   3'b110:out=in[6];
-   3'b111:out=in[7];
+   3'b000:out=in;
+   3'b001:out=in;
+   3'b010:out=in;
+   3'b011:out=in;
+   3'b100:out=in;
+   3'b101:out=in;
+   3'b110:out=in;
+   3'b111:out=in;
    endcase
    end
 endmodule
--- week 2 codes/demux_tb.v
+++ week 2 codes/demux_tb.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 
 module demux_tb;
-reg[7:0]in;
+reg in;
 reg[2:0]s;
 wire out;
 demux uut(.in(in),.s(s),.out(out));
 initial
 begin
 
-#10; s=3'b000; in=8'hAA;
-#10; s=3'b010; in=8'hBB;
-#40; s=3'b110; in=8'hCC;
-#20; s=3'b011; in=8'hDD;
+#10; s=3'b000; in=1'b0;
+#10; s=3'b010; in=1'b1;
+#40; s=3'b110; in=1'b1;
+#20; s=3'b011; in=1'b1;
 end
 endmodule
